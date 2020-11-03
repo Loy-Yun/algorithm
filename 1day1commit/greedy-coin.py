31814d1ca577e1685fb5656966d019cf9fca50a2
Soji Yun <yoonsoji@gmail.com>
2020-11-03 23:32:21 +0900

    day2

---
+++ 1day1commit/greedy-coin.py
@@ -0,0 +1,19 @@
+n, m = input().split()
+n = int(n)
+m = int(m)
+nlist = []
+count = 0
+
+for i in range(n):
+    nlist.append(eval(input()))
+    
+nlist.sort(reverse=True)
+
+for i in nlist:
+    if m <= 0: break
+    if i <= m:        
+        x = m//i
+        m = m - i*x       
+        count+=x
+
+print(count)
