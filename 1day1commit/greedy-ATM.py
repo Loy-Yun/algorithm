93bdc7f6828bf0fd1e6ab8060bb3006296b08c8e
윤소지 <yoonsoji@gmail.com>
2020-11-08 10:02:38 +0900

    day7

--- 1day1commit/greedy-ATM.py
+++ 1day1commit/greedy-ATM.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  6 20:17:55 2020
-
-@author: soji
-"""
-
 n = int(input())
-s = list(map(int, input().split()))
-num = 0
-s.sort()
+t = list(map(int, input().split()))
+
+sum = 0
+t.sort()
+
 for i in range(n):
     for j in range(i + 1):
-        num += s[j]
-print(num)
+        sum += t[j]
+print(sum)
