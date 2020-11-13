da9fee8e0d8da4a2db8b5253de8fb3f1711f7d04
윤소지 <yoonsoji@gmail.com>
2020-11-13 22:01:16 +0900

    day12

--- 1day1commit/insertionsort.py
+++ 1day1commit/insertionsort.py
@@ -1,4 +1,5 @@
 n = int(input("입력할 숫자의 개수는?: "))
+
 nlist = []
 
 for i in range(n):
@@ -9,6 +10,6 @@ for i in range(1, n):
         if nlist[j] < nlist[j-1]:
             nlist[j], nlist[j-1] = nlist[j-1], nlist[j]
         else: break
-
+        
 for i in nlist:
     print(i)
