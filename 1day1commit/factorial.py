693e2df30fadb6a4e78147bd3197e4d752e91719
윤소지 <yoonsoji@gmail.com>
2020-11-14 18:35:55 +0900

    day13

--- 1day1commit/factorial.py
+++ 1day1commit/factorial.py
@@ -1,9 +1,10 @@
-num = eval(input("0보다 크거나 같은 숫자를 입력하세요: "));
 
 def fac(n):
     if n == 1:
         return 1
     else: return n*fac(n-1)
+    
+num = eval(input("0보다 크거나 같은 숫자를 입력하세요: "));
 
 if(num == 0): print(1);
 else: print(fac(num));
