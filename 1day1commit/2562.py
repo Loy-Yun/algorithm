f58b3059fce42d73dfd8d889589d4ab77da92a3b
윤소지 <yoonsoji@gmail.com>
2021-03-10 00:47:01 +0900

    day10

---
+++ 1day1commit/2562.py
@@ -0,0 +1,15 @@
+import sys
+
+def getMax(n): 
+    num_list = list()
+    
+    for _ in range(n):
+        num_list.append(int(sys.stdin.readline().strip()))
+        max_num = max(num_list)
+        
+        print(max_num)
+        print(num_list.index(max_num) + 1)
+
+if __name__ == "__main__":
+            getMax(9)
+
